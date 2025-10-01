183c70588643947172f92a40c991cae85185d356
Nguyen Minh Chien <nmchien116@gmail.com>
2025-10-01 15:14:55 +0700

    feat:setup db entity

--- src/apis/index.ts
+++
@@ -1,8 +0,0 @@
-import UserRoute from '@/apis/users/users.route'
-import { Router } from 'express'
-
-const route = Router()
-
-route.use('/users', UserRoute)
-
-export default  route
---
+++ src/entities/base/DateTimeEntity.ts
@@ -0,0 +1,8 @@
+import { CreateDateColumn, UpdateDateColumn } from 'typeorm';
+
+export abstract class DateTimeEntity {
+    @CreateDateColumn({ type: 'timestamptz', name: 'created_at' })
+    public createdAt: Date;
+    @UpdateDateColumn({ type: 'timestamptz', name: 'updated_at' })
+    public updatedAt: Date;
+}
