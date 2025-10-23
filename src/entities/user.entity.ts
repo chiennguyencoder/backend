bc776a9c0d19b62e9462d1b2a62a6b2d498dd2f0
Nguyen Minh Chien <nmchien1106.chaicode@gmail.com>
2025-10-23 21:17:02 +0700

    fix: add google id into user entity

--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -1,47 +1,50 @@
-import { Column, Entity, JoinTable, ManyToMany, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, JoinTable, ManyToMany, OneToMany, PrimaryGeneratedColumn } from 'typeorm'
 
-import { DateTimeEntity } from './base/DateTimeEntity';
-import { CardMembers } from './card-member.entity';
-import { Comment } from './comment.entity';
-import { Notification } from './notification.entity';
-import { WorkspaceMembers } from './workspace-member.entity';
-import { Role } from './role.entity';
+import { DateTimeEntity } from './base/DateTimeEntity'
+import { CardMembers } from './card-member.entity'
+import { Comment } from './comment.entity'
+import { Notification } from './notification.entity'
+import { WorkspaceMembers } from './workspace-member.entity'
+import { Role } from './role.entity'
 
 @Entity('users')
 export class User extends DateTimeEntity {
-  @PrimaryGeneratedColumn('uuid')
-  public id: string;
+    @PrimaryGeneratedColumn('uuid')
+    public id: string
 
-  @Column({ type: 'varchar', unique: true, length: 255 })
-  public email: string;
+    @Column({ type: 'varchar', unique: true, length: 255 })
+    public email: string
 
-  @Column({ type: 'varchar', length: 255, nullable: true})
-  public password: string;
+    @Column({ type: 'varchar', length: 255, nullable: true })
+    public password: string
 
-  @Column({ type: 'varchar', length: 100, nullable: true })
-  public username: string;
+    @Column({ type: 'varchar', length: 100, nullable: true })
+    public username: string
 
-  @Column({ type: 'text', nullable: true })
-  public bio: string;
+    @Column({ type: 'text', nullable: true })
+    public bio: string
 
-  @Column({ type: 'varchar', length: 255, nullable: true })
-  public avatarUrl: string;
+    @Column({ type: 'varchar', length: 255, nullable: true })
+    public avatarUrl: string
 
-  @Column({ type: 'bool', nullable: false, default: false })
-  public isActive: boolean;
+    @Column({ type: 'varchar', length: 255, nullable: true })
+    public googleID: string
 
-  @OneToMany(() => WorkspaceMembers, (workspaceMember) => workspaceMember.user)
-  public workspaceMembers: WorkspaceMembers[];
+    @Column({ type: 'bool', nullable: false, default: false })
+    public isActive: boolean
 
-  @OneToMany(() => CardMembers, (cardMember) => cardMember.user)
-  public cardMembers: CardMembers[];
+    @OneToMany(() => WorkspaceMembers, (workspaceMember) => workspaceMember.user)
+    public workspaceMembers: WorkspaceMembers[]
 
-  @OneToMany(() => Comment, (comment) => comment.user)
-  public comments: Comment[];
+    @OneToMany(() => CardMembers, (cardMember) => cardMember.user)
+    public cardMembers: CardMembers[]
 
-  @OneToMany(() => Notification, (notification) => notification.user)
-  public notifications: Notification[];
-  @ManyToMany(() => Role, (role) => role.users)
-  @JoinTable()
-  public role: Role[];
+    @OneToMany(() => Comment, (comment) => comment.user)
+    public comments: Comment[]
+
+    @OneToMany(() => Notification, (notification) => notification.user)
+    public notifications: Notification[]
+    @ManyToMany(() => Role, (role) => role.users)
+    @JoinTable()
+    public role: Role[]
 }
