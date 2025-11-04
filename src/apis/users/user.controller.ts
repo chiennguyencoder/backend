12da3483f03cd019431a8585c664ebb41b14d9af
Nguyen Minh Chien <nmchien1106.chaicode@gmail.com>
2025-11-04 19:49:11 +0700

    refactor(user): clean up formatting and improve code readability in user controller

--- src/apis/users/user.controller.ts
+++ src/apis/users/user.controller.ts
@@ -1,43 +1,40 @@
-import { NextFunction, Response, Request } from 'express';
-import { errorResponse, successResponse } from '@/utils/response';
-import { Status } from '@/types/response';
-import {UserRepository} from './user.repository';
+import { NextFunction, Response, Request } from 'express'
+import { errorResponse, successResponse } from '@/utils/response'
+import { Status } from '@/types/response'
+import { UserRepository } from './user.repository'
 
-const userRepo = new UserRepository();
+const userRepo = new UserRepository()
 
 class UserController {
-    async getAll(req : Request, res : Response, next: NextFunction){
+    async getAll(req: Request, res: Response, next: NextFunction) {
         try {
             const users = await userRepo.findAll()
             return res.json(successResponse(Status.OK, 'Users fetched successfully', users))
-        }
-        catch(err){
+        } catch (err) {
             next(err)
         }
     }
 
-    async getUserByID(req : Request, res : Response, next: NextFunction){
-        try {   
+    async getUserByID(req: Request, res: Response, next: NextFunction) {
+        try {
             const { id } = req.params
             const user = await userRepo.findById(id)
-            if (user){
-                 res.json(successResponse(Status.OK, 'User fetched successfully', user));
-            }
-            else {
+            if (user) {
+                res.json(successResponse(Status.OK, 'User fetched successfully', user))
+            } else {
                 res.status(Status.NOT_FOUND).json(errorResponse(Status.NOT_FOUND, 'User not found'))
             }
-        }catch(err){
+        } catch (err) {
             next(err)
         }
     }
 
-    async createUser(req : Request, res : Response, next: NextFunction){
+    async createUser(req: Request, res: Response, next: NextFunction) {
         try {
             const { username, email, password } = req.body
-            await userRepo.createUser({ username, email, password})
+            await userRepo.createUser({ username, email, password })
             return res.json(successResponse(Status.CREATED, 'Create new user successfully!'))
-        }
-        catch(err){
+        } catch (err) {
             next(err)
         }
     }
