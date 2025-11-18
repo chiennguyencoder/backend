9f93be78ae1d49b3af09af11949585ba8d8e9afd
Nguyen Minh Chien <nmchien1106.chaicode@gmail.com>
2025-11-18 16:28:56 +0700

    feat(healthcheck): add health check endpoint for service status verification

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,6 +1,7 @@
 import UserRoute from '@/apis/users/users.route'
 import AuthRoute from '@/apis/auth/auth.route'
 import WorkspaceRoute from '@/apis/workspace/workspace.route'
+import HealthCheck from '@/apis/healthcheck/index'
 
 import { Router } from 'express'
 
@@ -9,5 +10,5 @@ const route = Router()
 route.use('/users', UserRoute)
 route.use('/auth', AuthRoute)
 route.use('/workspaces', WorkspaceRoute)
-
+route.use('/health', HealthCheck)
 export default route
