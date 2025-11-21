ae7d479cd608751eb528d834adb82c3874dc67c6
Nguyen Minh Chien <nmchien1106.chaicode@gmail.com>
2025-11-21 21:52:06 +0700

    feat: implement upload avatar with multer and cloudinary

--- src/middleware/upload.ts
+++ src/middleware/upload.ts
@@ -1,6 +1,22 @@
 import multer from 'multer'
+import { CloudinaryStorage } from 'multer-storage-cloudinary'
+import cloudinary from '@/config/cloundinary'
 
-const storage = multer.memoryStorage()
+const storage = new CloudinaryStorage({
+    cloudinary: cloudinary,
+    params: (req, file) => {
+        const folderPath = 'avatars/'
+        const fileExtension = file.originalname.split('.').pop();
+        const publicId = `${file.fieldname}-${req.user?.id}`;
+        return {
+            folder: folderPath,
+            format: fileExtension,
+            public_id: publicId,
+            transformation: [{ width: 500, height: 500, crop: 'limit' }]
+        };
+    }
+})
+
 
 export const upload = multer({
     storage,
